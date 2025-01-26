fca6352b5140da1ce97b9594c1632fabf192f869
Hjalte Tagmose <hjalte@tagmose.dk>
2025-01-26 13:42:45 +0100

    music

---
+++ Assets/Game/Source/Music.cs
@@ -0,0 +1,29 @@
+using FMOD.Studio;
+using FMODUnity;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using static UnityEngine.GraphicsBuffer;
+
+public class Music : MonoBehaviour
+{
+	[SerializeField]
+	private EventReference music;
+	private EventInstance musicInstance;
+
+	[SerializeField]
+	private float target = 0;
+	private float cur = 0;
+
+	private void Start()
+	{
+		musicInstance = AudioManager.Instance.CreateInstance(music);
+		musicInstance.start();
+		target = 0;
+	}
+
+	private void Update()
+	{
+		cur = Mathf.Lerp(cur, target, Time.deltaTime);
+		musicInstance.setParameterByName("switch", cur);
+	}
+}
